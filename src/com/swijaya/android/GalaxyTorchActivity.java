403bf93538bfd1c227c6d5034d0f3e7bd4bb9334
Santoso Wijaya <santoso.wijaya@gmail.com>
2012-01-12 15:54:35 -0800

    Skeletal glue.

--- src/com/swijaya/android/GalaxyTorchActivity.java
+++ src/com/swijaya/android/GalaxyTorchActivity.java
@@ -3,19 +3,34 @@ package com.swijaya.android;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class GalaxyTorchActivity extends Activity {
+public class GalaxyTorchActivity extends Activity implements OnClickListener {
+	
+	private CameraDevice mCameraDevice;
+	private boolean mIsTorchOn;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mCameraDevice = new GalaxyCameraDevice();
+        mIsTorchOn = false;
+        
+        Button button = (Button) findViewById(R.id.pressbutton);
+        button.setOnClickListener(this);
     }
     
-    /** Check if this device's feature list includes a camera. */
+    public void onClick(View v) {
+		mCameraDevice.turnCameraLED(!mIsTorchOn);
+	}
+
+	/** Check if this device's feature list includes a camera. */
     private boolean hasFeatureCamera(Context context) {
     	return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
