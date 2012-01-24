6733a354f716082848e1341d1370f5d71b494907
Santoso Wijaya <santoso.wijaya@gmail.com>
2012-01-24 12:14:26 -0800

    Start work on widget implementation.

---
+++ src/com/swijaya/galaxytorch/GalaxyTorchWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.swijaya.galaxytorch;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class GalaxyTorchWidgetProvider extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context context,
+			AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		// TODO Auto-generated method stub
+	}
+
+}
