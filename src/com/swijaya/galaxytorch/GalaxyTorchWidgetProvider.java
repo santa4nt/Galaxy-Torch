4ce26ab92224d017e433cbd6b6e0efabe68a1a6d
Santoso Wijaya <santoso.wijaya@gmail.com>
2012-01-24 22:17:05 -0800

    A working, dumb widget.

--- src/com/swijaya/galaxytorch/GalaxyTorchWidgetProvider.java
+++ src/com/swijaya/galaxytorch/GalaxyTorchWidgetProvider.java
@@ -3,14 +3,18 @@ package com.swijaya.galaxytorch;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.util.Log;
 
 public class GalaxyTorchWidgetProvider extends AppWidgetProvider {
 
-	@Override
-	public void onUpdate(Context context,
-			AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		// TODO Auto-generated method stub
-	}
+    private static final String TAG = GalaxyTorchWidgetProvider.class.getSimpleName();
+
+    @Override
+    public void onUpdate(Context context,
+            AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.v(TAG, "onUpdate");
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        // TODO Auto-generated method stub
+    }
 
 }
