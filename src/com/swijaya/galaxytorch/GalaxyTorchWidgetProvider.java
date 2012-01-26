7bafa1f6b0a240ee1075ee7805d8246c1db53888
Santoso Wijaya <santoso.wijaya@gmail.com>
2012-01-25 19:33:19 -0800

    Have the widget's button launch an activity.

--- src/com/swijaya/galaxytorch/GalaxyTorchWidgetProvider.java
+++ src/com/swijaya/galaxytorch/GalaxyTorchWidgetProvider.java
@@ -1,9 +1,12 @@
 package com.swijaya.galaxytorch;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 public class GalaxyTorchWidgetProvider extends AppWidgetProvider {
 
@@ -12,9 +15,26 @@ public class GalaxyTorchWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context,
             AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.v(TAG, "onUpdate");
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-        // TODO Auto-generated method stub
+        Log.v(TAG, "onUpdate");
+
+        // register an event handler (for button click) in this callback
+        final int n = appWidgetIds.length;
+        for (int i = 0; i < n; i++) {
+            int id = appWidgetIds[i];
+
+            // create an intent to launch GalaxyTorchWidgetHelperActivity
+            Intent intent = new Intent(context, GalaxyTorchActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // get the layout for the app widget (RemoteViews); then, attach an onClick
+            // listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.widgetbutton, pendingIntent);
+
+            // tell the manager to perform an update for the current widget
+            appWidgetManager.updateAppWidget(id, views);
+        }
     }
 
 }
