06eca4e2c3a04e789d6e2d1ca261ba4f12c8725c
Santoso Wijaya <santoso.wijaya@gmail.com>
2013-12-26 13:06:23 -0800

    Quick-n-dirty addition of a donate activity.

---
+++ src/com/swijaya/galaxytorch/BitcoinDonateActivity.java
@@ -0,0 +1,12 @@
+package com.swijaya.galaxytorch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BitcoinDonateActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.btcdonate);
+    }
+}
